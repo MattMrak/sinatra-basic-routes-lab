4362196aeb20f9bb45b1e73c380bd45d42670ab8
MattMrak <matthewmrakovcic@gmail.com>
2021-01-21 14:05:03 -0500

    commit tree 2619402b0fa96513248f7db945f3370dca9df9cc

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Matthew Mrakovcic."
+    end
+    get '/hometown' do 
+        "My hometown is Franklin Square in Long Island, NY."
+    end
+    get '/favorite-song' do 
+        "My favorite song is Electric Relaxation by A Tribe Called Quest."
+    end
 end
